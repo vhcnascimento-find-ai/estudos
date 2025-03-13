f01527812ba4b373c90ef48bb367529c2124e0a9
Victor Nascimento FAI <victor.nascimento@findai.com.br>
2025-03-13 18:58:16 -0300

    Update app.py to enhance audio file upload feature and modify title

--- app.py
+++ app.py
@@ -1,12 +1,15 @@
 import streamlit as st
 
-st.title('Hello World')
-st.write('This is a simple Streamlit app.')
+st.title('Transcrição de áudio')
 
 # Adiciona um botão para upload de múltiplos arquivos de áudio .ogg
-audio_files = st.file_uploader("Upload um ou mais arquivos de áudio .ogg", type=["ogg"], accept_multiple_files=True)
+audio_files = st.file_uploader("Anexe um ou mais arquivos de áudio .ogg", type=["ogg"], accept_multiple_files=True)
 
 if audio_files:
+    audio_names = [audio_file.name for audio_file in audio_files]
+    st.write("Arquivos enviados:")
+    st.write(audio_names)
+    
     for audio_file in audio_files:
-        st.audio(audio_file, format='audio/ogg')
         st.write(f"Arquivo: {audio_file.name}")
+        st.audio(audio_file, format='audio/ogg')
