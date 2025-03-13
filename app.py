ad9a26129e7a83d2ff2a9e32b6cd8ca023edccfe
Victor Nascimento FAI <victor.nascimento@findai.com.br>
2025-03-13 19:59:59 -0300

    Update audio processing interface and remove transcription functionality

--- app.py
+++ app.py
@@ -14,10 +14,7 @@ if not os.path.isfile(ffmpeg_path):
 else:
     st.success("FFmpeg configurado corretamente.")
 
-st.title('Transcrição de áudio')
-
-# Carrega o modelo Whisper
-model = whisper.load_model("small")  # Você pode escolher outros modelos como 'small', 'medium', 'large'
+st.title('Reprodução de Áudio')
 
 # Adiciona um botão para upload de um único arquivo de áudio .ogg
 audio_file = st.file_uploader("Anexe um arquivo de áudio .ogg", type=["ogg"])
@@ -25,18 +22,3 @@ audio_file = st.file_uploader("Anexe um arquivo de áudio .ogg", type=["ogg"])
 if audio_file:
     st.write(f"Arquivo enviado: {audio_file.name}")
     st.audio(audio_file, format='audio/ogg')
-    
-    # Salva o arquivo de áudio enviado
-    with open("uploaded_audio.ogg", "wb") as f:
-        f.write(audio_file.getbuffer())
-    
-    # Converte o arquivo de áudio para .wav
-    sound = AudioSegment.from_ogg("uploaded_audio.ogg")
-    sound.export("audio.wav", format="wav")
-    
-    # Transcreve o áudio usando o modelo Whisper
-    result = model.transcribe("audio.wav")
-    texto_transcrito = result["text"]
-    
-    # Exibe o texto transcrito na tela
-    st.text_area("Texto Transcrito", texto_transcrito, height=300)
