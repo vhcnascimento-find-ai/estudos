61b6c35a96f831a8103fb62665ae10ef670aa8e2
Victor Nascimento FAI <victor.nascimento@findai.com.br>
2025-03-14 15:36:32 -0300

    Enhance audio file support by adding OGG format and improve playback functionality

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import speech_recognition as sr
 from io import BytesIO
+from pydub import AudioSegment
+import tempfile
 
 # Inicializa o reconhecedor
 recognizer = sr.Recognizer()
@@ -8,20 +10,29 @@ recognizer = sr.Recognizer()
 st.title("Transcrição de Áudio")
 
 # Carrega o arquivo de áudio
-uploaded_file = st.file_uploader("Escolha um arquivo de áudio", type=["wav", "mp3"])
+uploaded_file = st.file_uploader("Escolha um arquivo de áudio", type=["wav", "mp3", "ogg"])
 
 if uploaded_file is not None:
-    # Reproduzir o áudio carregado
-    st.audio(uploaded_file, format=f"audio/{uploaded_file.type.split('/')[-1]}")
+    # Converte o arquivo para WAV se for OGG
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp_wav_file:
+        if uploaded_file.name.endswith(".ogg"):
+            audio_data = BytesIO(uploaded_file.read())
+            audio = AudioSegment.from_file(audio_data, format="ogg")
+            audio.export(temp_wav_file.name, format="wav")
+            audio_path = temp_wav_file.name
+        else:
+            audio_path = uploaded_file
+
+    # Reproduzir o áudio convertido
+    st.audio(audio_path, format="audio/wav")
     
     if st.button("Iniciar Transcrição"):
         # Converte o arquivo carregado para um formato que o SpeechRecognition pode usar
-        audio_data = BytesIO(uploaded_file.read())
-        audio_file = sr.AudioFile(audio_data)
+        audio_file = sr.AudioFile(audio_path)
 
         with audio_file as source:
             # Ajusta o nível de ruído por mais tempo para melhorar a precisão
-            recognizer.adjust_for_ambient_noise(source, duration=1)
+            recognizer.adjust_for_ambient_noise(source, duration=1.5)
             audio = recognizer.record(source)
 
         # Transcreve o áudio com melhores configurações
